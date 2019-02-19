b070e2431e3bc9772131dffcf6a42b3e20046b81
James Bryant <>
2019-02-19 05:54:33 -0500

    AWS Endpoint.

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy_utils import create_database
 from sqlalchemy_utils import drop_database
 from elios_app.models import Person, Address, CriminalRecord, Base
 
-engine = create_engine('postgresql+psycopg2://flask_dev:letmein123@localhost/elios')
+engine = create_engine('postgresql+psycopg2://developer:letmein123@flask-test.cjqpvm7upos7.us-east-2.rds.amazonaws.com:5432/flask-test')
 
 if database_exists(engine.url):
     print("Dropping DB before creating")
